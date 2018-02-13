9ffe6c8262acfa505b2e56bb69484c95a96774f2
Michael Smith <mes258@case.edu>
2018-02-13 14:02:46 -0500

    The M_list parameters are finally fixed - don't pass while, retrun, etc to functions

--- Interpreter1.rkt
+++ Interpreter1.rkt
@@ -1,5 +1,5 @@
 ;Interpreter1 below:
-;Vincent Portell, Michael Smith, ____
+;Vincent Portell, Michael Smith
 
 (require "simpleParser.scm")
 
@@ -16,74 +16,86 @@
 ;going forward. 
 (define p1 (build-path (current-directory) "code.txt"))
 
-
 ;(parser p1)
 (define lis (parser p1))
+
 ;need to pass lis into M_list
 ;go through the list of statements returned by interpret
 (define M_list
   (lambda (lis s)
     (cond
       ((null? lis) s)
-      ((and (eq? (type lis) 'var) (null? (caddr lis))) (cons (M_state_decl1 (fir lis) (sec lis) s) (M_list (cdr lis) s)))
-      ((eq? (type lis) 'var) (cons (M_state_decl2 (fir lis) (sec lis) (thr lis) s) (M_list (cdr lis) s)))
-      ((eq? (type lis) 'while) (cons (M_state_while (fir lis) (sec lis) (thr lis) (frth lis) s) (M_list (cdr lis) s)))
-      ((eq? (type lis) 'return) (cons (M_state_return (fir lis) (sec lis)) (M_list (cdr lis) s)))
-      ((and (eq? (type lis) 'if) (null? (cdddar lis))) (cons (M_state_if (fir lis) (sec lis) s) (M_list (cdr lis) s)))
-      ((eq? (type lis) 'if) (cons (M_state_if_else (fir lis) (sec lis) (thr lis) s) (M_list (cdr lis) s)))
-      (else lis)))) ; need to fix parameters of functions - get each part of (car lis) not just (car lis) for the parameter
+      ((and (eq? (type lis) 'var) (null? (caddr lis)))
+       (cons (M_state_decl1 (fir lis) s) (M_list (cdr lis) s)))
+      ((eq? (type lis) 'var)
+       (cons (M_state_decl2 (fir lis) (sec lis) s) (M_list (cdr lis) s)))
+      ((eq? (type lis) 'while)
+       (cons (M_state_while (fir lis) (sec lis) s) (M_list (cdr lis) s)))
+      ((eq? (type lis) 'return)
+       (cons (M_state_return (fir lis)) (M_list (cdr lis) s)))
+      ((and (eq? (type lis) 'if) (null? (cdddar lis)))
+       (cons (M_state_if (fir lis) (sec lis) s) (M_list (cdr lis) s)))
+      ((eq? (type lis) 'if)
+       (cons (M_state_if_else (fir lis) (sec lis) (thr lis) s) (M_list (cdr lis) s)))
+      (else lis)))) ; Parameters should be all fixed. Need to confirm M_list (car lis) works given defined lis above. 
 
 ;abstraction for M_list
-(define type caar);type of line
-(define fir caar);first element 
-(define sec cadr);second element
-(define thr caddr);third element
-(define frth cadddr);fourth element
+;note: we are ignoring the first part of the line, such as "while" or "if". In those cases, (fir lis) refers to <condtion>.
+(define type caar);type of call
+(define fir cadr);First parameter
+(define sec caddr);Second parameter
+(define thi cadddr);Third parameter
 
 
-;need M_bool, M_state, M_value for:
+;need M_state for: 
 ;variable declaration 	(var variable) or (var variable value)
 ;assignment 	(= variable expression)
 ;return 	(return expression)
 ;if statement 	(if conditional then-statement optional-else-statement)
 ;while statement 	(while conditional body-statement)
 
-;M_State_stuff
-(define M_state_decl1
-  (lambda (var variable s)
+;Need M_value for:
+;math operators
+
+;Need M_bool for:
+;conditonals 
+
+;M_state
+(define M_state_decl1 ;add variable to state with value null
+  (lambda (variable s)
     (cond
       )))
 
-(define M_state_decl2
-  (lambda (var variable value s)
+(define M_state_decl2 ;add variable to state with value val
+  (lambda (variable value s)
     (cond
       )))
 
-(define M_state_while
-  (lambda (while condit body s) 
+(define M_state_while ;modify the state as the body says
+  (lambda (condit body s) 
     (cond
       )))
 
-(define M_state_return
-  (lambda (return exp)
+(define M_state_return ;return exp
+  (lambda (exp)
     (cond
       ((null? exp) '())
       (else (M_bool_op exp)))))
 
-(define M_state_if_else
+(define M_state_if_else ;check the condition and modify s based on the value of condition 
   (lambda (condition then else s)
     (if (M_bool_op condition)
         (M_list then s)
         (M_list else s))))
 
-(define M_state_if
+(define M_state_if ; if true, modify based on then else do nothing
   (lambda (condition then s)
     (if (M_bool_op condition)
         (M_list then s)
         `())))
 
 
-;M_value_stuff
+;M_value
 
 (define M_value_op
   (lambda (lis)
@@ -97,7 +109,7 @@
       ((eq? (operator lis) '%) (remainder (M_value_op (operand1 lis)) (M_value_op (operand2 lis))))
       (else (error 'badoperation "Unknown operator")))))
 
-;M_boolean_stuff
+;M_boolean
 
 (define M_bool_op
   (lambda (lis)
