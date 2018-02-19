73e41665ea26674d027af1460079c6b8c6b5a5bb
Vincent Portelli <vinport16@gmail.com>
2018-02-18 22:31:07 -0500

    made it so things like 'while (x=x+1) < 10' work

--- Interpreter1.rkt
+++ Interpreter1.rkt
@@ -28,21 +28,31 @@
   (lambda (lis s)
     (cond
       ((null? lis) s)
-      ((and (eq? (caar lis) 'var) (null? (cddar lis)))
+      ((and (not (list? (car lis))) (null? (cdr lis))) s)
+      ((and (eq? (car lis) 'var) (null? (cddar lis)))
        (M_list (cdr lis) (M_state_decl1 (fir lis) s)))
       ((eq? (type lis) 'var)
        (M_list (cdr lis) (M_state_decl2 (fir lis) (sec lis) s)))
       ((eq? (type lis) '=)
        (M_list (cdr lis) (M_state_assign (fir lis) (sec lis) s)))
       ((eq? (type lis) 'while)
-       (M_list (cdr lis) s) (M_state_while (fir lis) (cddar lis) s))
+       (M_list (cdr lis) (M_state_while (fir lis) (cddar lis) s)))
       ((eq? (type lis) 'return)
-       (M_list (cdr lis) (M_state_return (fir lis) s)))
-      ((and (eq? (type lis) 'if) (null? (cdddar lis)))
-       (M_list (cdr lis) (M_state_if (fir lis) (sec lis) s)))
+       (M_value_op (cadar lis) s))
+      ((eq? (type lis) 'if)
+       (if (null? (cdddar lis))
+           (M_list (cdr lis) (M_state_if (ifcond (car lis)) (list (ifdo (car lis))) s))
+           (M_list (cdr lis) (M_state_if_else (ifcond (car lis)) (list (ifdo (car lis))) (list (ifelsedo (car lis))) s))))
       ;((eq? (type lis) 'if)
        ;(cons (M_state_if_else (fir lis) (sec lis) (thr lis) s) (M_list (cdr lis) s)))
-      (else lis)))) ; Parameters should be all fixed. Need to confirm M_list (car lis) works given defined lis above. 
+      ((or (eq? (type lis) '==)
+           (eq? (type lis) '!=)
+           (eq? (type lis) '>=)
+           (eq? (type lis) '<=)
+           (eq? (type lis) '>)
+           (eq? (type lis) '<))
+       (M_list (cdr lis) (M_list (list (operand2 (car lis))) (M_list (list (operand1 (car lis))) s))))
+      (else s))))
 
 ;abstraction for M_list
 ;note: we are ignoring the first part of the line, such as "while" or "if". In those cases, (fir lis) refers to <condtion>.
@@ -50,6 +60,9 @@
 (define fir cadar);First parameter
 (define sec caddar);Second parameter
 ;(define thi cadddar);Third parameter
+(define ifcond cadr)
+(define ifdo caddr)
+(define ifelsedo cadddr)
 
 
 ;need M_state for: 
@@ -88,13 +101,13 @@
        (list (car s) (cons (M_value_op exp s) (cdadr s))))
       (else
        (cons (car s) (list (cons (caadr s) (cadr (M_state_assign variable exp (cons (cdar s) (list (cdadr s)))) )))
-                  )))))      
+                  )))))
 
 (define M_state_while ;modify the state as the body says
-  (lambda (condit body s) 
-    (if (M_bool_op condit s)
-        (M_state_while condit body (M_list body s))
-        s)))
+  (lambda (condition body s) 
+    (if (M_bool_op condition s)
+        (M_state_while condition body (M_list body (M_list (list condition) s)))
+        (M_list (list condition) s))))
 
 (define M_state_return ;return exp
   (lambda (exp s)
@@ -107,15 +120,16 @@
 
 (define M_state_if_else ;check the condition and modify s based on the value of condition 
   (lambda (condition then else s)
-    (if (M_bool_op condition)
-        (M_list then s)
-        (M_list else s))))
+    (if (M_bool_op condition s)
+        (M_list then (M_list (list condition) s))
+        (M_list else (M_list (list condition) s)))))
 
-(define M_state_if ; if true, modify based on then else do nothing
+(define M_state_if ; if true, modify based on then. Otherwise do nothing
   (lambda (condition then s)
-    (if (M_bool_op condition)
-        (M_list then s)
-        `())))
+    (if (M_bool_op condition (M_list (list condition) s))
+        (M_list then (M_list (list condition) s))
+        s
+        )))
 
 
 ;M_value
@@ -126,7 +140,6 @@
       ((null? lis) '())
       ((number? lis) lis)
       ((not (null? (varvalue lis s))) (varvalue lis s))
-      ;((number? (car lis)) (car lis))
       ((null? (car lis)) (varvalue lis s))
       ((null? (cdr lis)) (M_value_op lis (car s)))
       ((eq? (operator lis) '+) (+ (M_value_op (operand1 lis) s) (M_value_op (operand2 lis) s)))
@@ -134,18 +147,10 @@
       ((eq? (operator lis) '*) (* (M_value_op (operand1 lis) s) (M_value_op (operand2 lis) s)))
       ((eq? (operator lis) '/) (quotient (M_value_op (operand1 lis) s) (M_value_op (operand2 lis) s)))
       ((eq? (operator lis) '%) (remainder (M_value_op (operand1 lis) s) (M_value_op (operand2 lis) s)))
+      ((eq? (operator lis) '=) (M_value_op (operand2 lis) s ))
       (else (error 'badoperation "Unknown operator")))))
 
-(define isvariable? ; gives weather a variable was declared
-  (lambda (name s)
-    (cond
-      ((not (null? (car s)))
-       (if(eq? (caar s) name )
-          true
-          (isvariable? name (cons (cdar s) (list(cdadr s))))))
-      (else false))))
-
-(define varvalue ; gives the value of a variable given a state
+(define varvalue ; gives the value of a variable given a state [if doesn't exist, gives null]
   (lambda (name s)
     (cond
       ((not (null? (car s)))
@@ -169,9 +174,9 @@
       ((eq? (operator lis) '>) (> (M_value_op (operand1 lis) s) (M_value_op (operand2 lis) s)))
       ((eq? (operator lis) '<) (< (M_value_op (operand1 lis) s) (M_value_op (operand2 lis) s)))
       ((eq? (operator lis) '!=) (not (eq? (M_value_op (operand1 lis) s) (M_value_op (operand2 lis) s))))
-      ((eq? (operator lis) '||) (or (M_value_op (operand1 lis) s) (M_value_op (operand2 lis) s)))
-      ((eq? (operator lis) '&&) (and (M_value_op (operand1 lis) s) (M_value_op (operand2 lis) s)))
-      ((eq? (operator lis) '!) (not (M_value_op (operand1 lis) s)))
+      ((eq? (operator lis) '||) (or (M_bool_op (operand1 lis) s) (M_bool_op (operand2 lis) s)))
+      ((eq? (operator lis) '&&) (and (M_bool_op (operand1 lis) s) (M_bool_op (operand2 lis) s)))
+      ((eq? (operator lis) '!) (not (M_bool_op (operand1 lis) s)))
       (else (M_value_op lis)))))
 
 ;abstraction for M_value_op and M_bool_op
@@ -183,7 +188,9 @@
   (lambda (x)
     (and (not (pair? x)) (not (null? x)) (not (list? x))) ))
 
-(M_list '( (var x 11) (var y (+ 2 2)) (while (!= y x) (= y (+ y 1)))) '())
+(M_list '( (var x 11) (var y (+ 2 2)) (while (!= y x) (= y (+ y 1))) (if (== y 11) (= y 420) (= y 0) ) ) '())
+(M_list '( (var x 9) (= x (+ x 1)) (if (== (= x (+ x 1)) 88) (var y 1) (var y 2)) (= y 1)) '())
+(M_list '( (var x 10) (if (== x 17) (= x 99) (var y 4)) (while (!= (= x (+ 1 x)) 99) (= x x)) ) '())
 
 ;(M_state_assign 'y 10 (M_state_decl1 'y (M_state_decl2 'x 7 '())))
 
